416c3da06ea7b8f5ef9c901994d601c0846fa562
amit15091996 <amitdewangan199@gmail.com>
2022-07-11 12:06:10 +0530

    hotspot

--- src/main/java/com/excel/controller/ExcelController.java
+++ src/main/java/com/excel/controller/ExcelController.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +18,6 @@ import com.excel.helper.Helper;
 import com.excel.service.ExcelService;
 
 @RestController
-@CrossOrigin("*")
 public class ExcelController {
 
 	@Autowired	
